69994de876f53ae425f59eea9a0f5f1a7c43cde3
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2022-07-07 13:19:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX= 0;
+noseY= 0;
 function preload(){
-
+clownNose=loadImage("https://i.postimg.cc/tJs4rJvs/image.png");
 }
 function setup(){
 canvas = createCanvas(300,300);
@@ -13,18 +15,24 @@ posenet.on('pose',gotPoses);
 }
 function draw(){
 image(video,0,0,300,300);
+//fill(255,0,0);
+//stroke(255,0,0);
+//circle(noseX,noseY,20);
+image(clownNose,noseX-15,noseY+5,40,40);
 }
 function takeSnapshot(){
-    save("mustache+lipstickFilter.png");
+    save("clownNoseFilter.png");
 }
 
 function modelLoaded(){
-     console.log("Posenet is initialized");
+    console.log("PoseNet is initialized");
 }
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("noseX = " + results[0].pose.nose.x)
-        console.log("noseY = " + results[0].pose.nose.y)
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("noseX = " + noseX); 
+        console.log("noseY = " + noseY); 
     }
 }
